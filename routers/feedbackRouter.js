161b16d655e82768506bad488278200be38e4e4a
Md. Rifat Islam <xxxxrifatxxxx@gmail.com>
2022-03-24 18:06:38 +0600

    Feedback curd Done

--- routers/feedbackRouter.js
+++ routers/feedbackRouter.js
@@ -27,5 +27,91 @@ feedbackRouter.post('/', async (req, res) => {
     };
 });
 
+// Update Feedback
+feedbackRouter.put('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    // Submit To Database
+    try {
+        const data = await Feedback.findByIdAndUpdate(id, req.body);
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success"
+        });
+    }
+
+    catch (error) {
+        res.status(error.status || 500);
+        res.send({
+            error: error.message
+        });
+    };
+});
+
+// Delete Feedback
+feedbackRouter.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    // Submit To Database
+    try {
+        const data = await Feedback.findByIdAndRemove(id);
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success"
+        });
+    }
+
+    catch (error) {
+        res.status(error.status || 500);
+        res.send({
+            error: error.message
+        });
+    };
+});
+
+// Single Feedback
+feedbackRouter.get('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    // Submit To Database
+    try {
+        const data = await Feedback.findById(id).exec();
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success"
+        });
+    }
+
+    catch (error) {
+        res.status(error.status || 500);
+        res.send({
+            error: error.message
+        });
+    };
+});
+
+// Get All Feedback
+feedbackRouter.get('/', async (req, res) => {
+    // Submit To Database
+    try {
+        const data = await Feedback.find({});
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success"
+        });
+    }
+
+    catch (error) {
+        res.status(error.status || 500);
+        res.send({
+            error: error.message
+        });
+    };
+});
+
 // Export
 module.exports = feedbackRouter;
